b3fbd8842abe34db64eb49c9f84d329ae7e15ca3
roshankumbharx <roshankumbhar9987@gmail.com>
2026-01-26 23:06:19 +0530

    load vectorstore

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -21,9 +21,52 @@ os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
 UPLOAD_DIR = "./uploaded_docs"
 os.mkdir(UPLOAD_DIR,exist_ok=True)
 
-# pinecone init
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
 spec = ServerlessSpec(cloud='aws',region=PINECONE_ENV)
 
-# fetching all the existing indexes
+
+existing_index = [i['name'] for i in pc.list_indexes()]
+
+if PINECONE_INDEX_NAME not in existing_index:
+    pc.create_index(
+        name=PINECONE_INDEX_NAME,
+        dimension=768,
+        spec=spec,
+        metric="dotproduct",
+    )
+    
+    while not pc.describe_index(PINECONE_INDEX_NAME).status['ready']:
+        time.sleep(1)
+        
+index  = pc.Index(PINECONE_INDEX_NAME)
+
+
+def load_vectorstore(uploaded_file):
+    embed_model=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    file_paths=[]
+    
+    for file in uploaded_file:
+        save_path=Path(UPLOAD_DIR)/file.filename
+        with open(save_path,'wb') as f:
+            f.write(file.file.read())
+        file_paths.append(str(save_path))
+        
+    for file_path in file_paths:
+        loader=PyPDFLoader(file_path)
+        document=loader.load()
+        
+        splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+        chunks = splitter.split_documents(document)
+        
+        texts = [chunk.page_content for chunk in chunks]
+        metadata = [chunk.metadata for chunk in chunks]
+        ids = [f"{Path(file_path).stem}-{i}" for i in range(len(chunks))]
+
+        print(f"Embedding chunks")
+        embedding=embed_model.embed_documents(texts)
+        
+        print("Upserting documents")
+        with tqdm(total=len(embedding),desc="Upserting to Pinecone") as progress:
+            index.upsert(vectors=zip(ids,embedding,metadata))
+            progress.update(len(embedding))
