c4573a46658e12492550da469a9c58e54c4bcb35
roshankumbharx <roshankumbhar9987@gmail.com>
2026-01-30 23:16:34 +0530

    ask_question router completed

--- server/routers/ask_question.py
+++ server/routers/ask_question.py
@@ -19,6 +19,38 @@ async def ask_question(question:str=Form(...)):
     try:
         logger.info(f"User query:{question}")
         
+        # embed query and retrieve top k results
+        pc=Pinecone(api_key=os.environ("PINECONE_API_KEY"))
+        index=pc.Index(os.environ("PINECONE_INDEX_NAME"))
+        embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        embed_query = embed_model.embed_query(question)
+        res=index.query(vector=embed_query,top_k=3,include_metadata=True)
+        
+        docs =[
+            Document(
+                page_content=match["metadata"].get("text",""),
+                metadata=match["metadata"]
+            ) for match in res['matches']
+        ]
+        
+        class SimpleRetriever(BaseRetriever):
+            tags:Optional[List[str]] = Field(default_factory=list)
+            metadata:Optional[dict] = Field(default_factory=dict)
+            
+            def __init__(self,documents:List[Document]):
+                super().__init__()
+                self._docs=documents
+                
+            def _get_relevant_documents(self,query:List[Document]):
+                return self._docs
+            
+        retriever = SimpleRetriever(docs)
+        chain = get_llm_chain(retriever)
+        result = query_chain(chain,question)
+        
+        logger.info("Query is succesful")
+        return result
+        
     
     except Exception as e:
         logger.exception('Error processing question')
